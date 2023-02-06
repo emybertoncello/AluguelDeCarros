4a28a4cd720f91c04deb9c95ca51ded767eb7031
Emelin Bertoncello <88146420+emybertoncello@users.noreply.github.com>
2023-02-06 18:35:33 -0300

    Add files via upload

---
+++ ExSemInterface/ExSemInterface/Services/BrazilTaxService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace ExSemInterface.Services
+{
+    class BrazilTaxService
+    {
+        public double Tax(double amount)
+        {
+            if (amount <= 100.0)
+            {
+                return amount * 0.2;
+            }
+            else
+            {
+                return amount * .15;
+            }
+        }
+    }
+}
