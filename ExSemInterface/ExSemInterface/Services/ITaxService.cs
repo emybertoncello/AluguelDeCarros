88f629b0c928f0a48fe293aa4935437c5d150198
Emelin Bertoncello <88146420+emybertoncello@users.noreply.github.com>
2023-02-07 11:08:30 -0300

    Commit utilizando boas práticas SOLID
    Com uma mudança simples, criei uma interface, onde o código fica mais limpo e previne futuros erros usando boas práticas de SOLID.

---
+++ ExSemInterface/ExSemInterface/Services/ITaxService.cs
@@ -0,0 +1,7 @@
+﻿namespace ExSemInterface.Services
+{
+    interface ITaxService
+    {
+        double Tax(double amount);
+    }
+}
--- ExSemInterface/ExSemInterface/Services/RentalService.cs
+++ ExSemInterface/ExSemInterface/Services/RentalService.cs
@@ -8,12 +8,13 @@ namespace ExSemInterface.Services
         public double PricePerHour { get; private set; }
         public double PricePerDay { get; private set; }
 
-        private BrazilTaxService _brazilTaxService = new BrazilTaxService();
+        private ITaxService _taxService;
 
-        public RentalService(double pricePerHour, double pricePerDay)
+        public RentalService(double pricePerHour, double pricePerDay, ITaxService taxService)
         {
-            PricePerHour = pricePerHour;
+            PricePerHour = pricePerHour; 
             PricePerDay = pricePerDay;
+            _taxService = taxService;
         }
         public void ProcessInvoice(CarRental carRental)
         {
@@ -28,7 +29,7 @@ namespace ExSemInterface.Services
             {
                 basicPayment = PricePerDay * Math.Ceiling(duration.TotalDays);
             }
-            double tax = _brazilTaxService.Tax(basicPayment); 
+            double tax = _taxService.Tax(basicPayment); 
 
             carRental.Invoice = new Invoice(basicPayment, tax);
         }
